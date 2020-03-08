a4246c2f70295edc523a352c34fd3d1afd7fc168
Deakcor <deak24cor@gmail.com>
2020-03-08 15:08:47 +0100

    entity/player movement + dash

---
+++ src/scenes/entity/entity.gd
@@ -0,0 +1,54 @@
+extends KinematicBody2D
+
+class_name Entity
+
+const G=9.81
+
+var acceleration:=10
+var speed:=100
+
+var dash_power:=6
+var dash_dec:=6
+var time_dash:=0.5
+var time_dash_recover:=0.2
+var can_dash:=true
+var dashing:=false
+
+var atking:=false
+
+var _velocity:= Vector2.ZERO
+var _look_dir:=Vector2.RIGHT
+var _height:=0.0
+
+func get_direction() -> Vector2:
+	return Vector2.ZERO
+	
+func _physics_process(delta: float) -> void:
+	var dir:=get_direction().normalized()
+	if !dashing and !atking:
+		_velocity = lerp(_velocity,speed*dir,acceleration*delta)
+	elif dashing:
+		_velocity=lerp(_velocity,Vector2.ZERO,delta*dash_dec)
+	move_and_slide(_velocity)
+	animation()
+
+func animation():
+	$sprite.y=_height
+
+func dash():
+	var dir:=get_direction()
+	if can_dash and !dashing:
+		atking=false
+		if dir!=Vector2.ZERO:
+			_velocity=dir.normalized()*speed*dash_power
+			dashing=true
+			can_dash=false
+			$timer_dash.start(time_dash)
+
+
+func _on_timer_dash_timeout():
+	if dashing:
+		dashing=false
+		$timer_dash.start(time_dash_recover)
+	else:
+		can_dash=true
---
+++ src/scenes/entity/player.gd
@@ -0,0 +1,11 @@
+extends Entity
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+
+func _input(event):
+	if event.is_action_pressed("dash"):
+		dash()
